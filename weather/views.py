ef741243556c64aa528ff769b64f127ca8f9c085
Robert Milinski <robimilinski@gmail.com>
2021-08-20 13:09:18 +1000

    mycity and defined cities working

--- weather/views.py
+++ weather/views.py
@@ -2,38 +2,50 @@ from django.shortcuts import render
 import urllib.request
 import json
 
-city_details = ['Canberra', 'Sydney']
+class CityWeather:
+    def __init__(self, name, lat, lon):
+      self.name = name
+      self.lat = lat
+      self.lon = lon
+      self.temp_min = 0
+      self.temp_max = 0
+      self.humidity = 0
 
-canberra_details = {"name": "Canberra", "latitude": "-35", "longitude": "149"}
-sydney_details = {"name": "Sydney", "latitude": "-34", "longitude": "151"}
+canberra_details = CityWeather("Canberra", "-35", "149")
+seattle_details = CityWeather("Seattle", "48", "-121")
+london_details = CityWeather("London", "51", "0")
+
+displayed_cities = (canberra_details, seattle_details, london_details)
+
+def get_city_weather(mycity):
+    lat = mycity.lat
+    lon = mycity.lon
+
+    app_key = '75523f4c2e67ef3ab67afe0d532a2795'
+    URL = 'http://api.openweathermap.org/data/2.5/weather?'
+
+    if mycity.name != '':
+        URL += 'q=' + mycity.name + '&units=metric&appid=' + app_key
+    else:
+        URL += 'lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + app_key
+
+    source = urllib.request.urlopen(URL).read()
+
+    raw_weather_data = json.loads(source)
+    mycity.temp_min = str(raw_weather_data['main']['temp_min']) + ' °C'
+    mycity.temp_max = str(raw_weather_data['main']['temp_max']) + ' °C'
+    mycity.humidity = str(raw_weather_data['main']['humidity']) + ' %'
+
+def get_displayed_cities_weather(cities_list):
+    for city in cities_list:
+        city_weather = get_city_weather(city)
 
 def weatherapp(request):
-        if request.method == "POST":
-            app_key = '75523f4c2e67ef3ab67afe0d532a2795'
-
-            URL = 'http://api.openweathermap.org/data/2.5/weather?'
-
-            city = request.POST['city']
-            latitude = request.POST['latitude']
-            longitude = request.POST['longitude']
-
-            if city != '':
-                URL += 'q=' + city + '&units=metric&appid=' + app_key
-            else:
-                URL += 'lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=' + app_key
-            
-            source = urllib.request.urlopen(URL).read()
-
-            raw_weather_data = json.loads(source)
-
-            data = {
-                "name": str(raw_weather_data['name']),
-                "temp_min": str(raw_weather_data['main']['temp_min']) + ' °C',
-                "temp_max": str(raw_weather_data['main']['temp_max']) + ' °C',
-                "humidity": str(raw_weather_data['main']['humidity']) + ' %',
-            }
-            print(data)
-        else:
-            data = {}
-
-        return render(request, 'weather/weatherapp.html', data)
+    if request.method == "POST":
+        input_city = CityWeather(request.POST['city'], request.POST['latitude'], request.POST['longitude'])
+        get_city_weather(input_city)
+    #else:
+    #    print('hello world')
+    #return render(request, 'weather/weatherapp.html', data)
+    get_displayed_cities_weather(displayed_cities)
+    return render(request, 'weather/weatherapp.html', {'displayed_cities': displayed_cities})
